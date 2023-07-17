a93dce2cb8b7b59c54bfe1af4e602eb813e4c00d
unknown <joao.bacelar22@gmail.com>
2023-07-17 13:33:41 -0300

    Inicial

---
+++ src/test/java/com/bacelar/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bacelar.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
